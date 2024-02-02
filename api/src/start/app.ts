b0cd6ffbb62e94711e86fa3f2b2cee3110a254aa
Emiliano Perez <emiperez.dev@gmail.com>
2024-02-02 12:18:30 -0600

    Handling express errors

--- api/src/start/app.ts
+++ api/src/start/app.ts
@@ -1,6 +1,7 @@
 import express, { Application } from "express";
 import morgan from "morgan";
 import usersRouter from "../routes/users.routes";
+import error from "../middleware/error";
 
 const app: Application = express();
 
@@ -9,5 +10,7 @@ app.use(morgan("dev"));
 
 const baseURL = "/api";
 app.use(baseURL + "/users", usersRouter);
+
+app.use(error)
 
 export default app;
